980b06589ad6086cc26cb9c716151d87e2b0ee9d
DimaK128 <dmytro.kolesnyk@auk.edu.ua>
2024-05-16 18:57:06 +0300

    !

--- src/main/java/com/example/inheritancefeatures/Main.java
+++ src/main/java/com/example/inheritancefeatures/Main.java
@@ -1,87 +1,94 @@
 package com.example.inheritancefeatures;
 
-// 2.1.1. Create superclass (base class / parent class)
+// 2.1.1. Create superclass
 class SuperClass {
-    // 2.1.3. Instance variables with different access modifiers
-    private int privateVar;
-    protected String protectedVar;
-    public double publicVar;
+    private int privateVar = 10;
+    protected int protectedVar = 20;
+    public int publicVar = 30;
 
-    // Constructor
-    public SuperClass(int privateVar, String protectedVar, double publicVar) {
-        this.privateVar = privateVar;
-        this.protectedVar = protectedVar;
-        this.publicVar = publicVar;
+    public void display() {
+        System.out.println("Inside SuperClass");
     }
 }
 
 // 2.1.8. Declare abstract class
 abstract class MyAbstractClass {
-    // Abstract method
     abstract void abstractMethod();
 }
 
-// 2.1.1. Create subclass (child class) which inherits the superclass
+// 2.1.8. Implement abstract method
 class SubClass extends SuperClass {
-    // Constructor
-    public SubClass(int privateVar, String protectedVar, double publicVar) {
-        super(privateVar, protectedVar, publicVar); // 2.1.7. Call superclass constructor using super()
+    // 2.1.8. Implement abstract method
+    void abstractMethod() {
+        System.out.println("Abstract method implemented");
+    }
+
+    // 2.1.3. Access superclass instance variables
+    void accessVariables() {
+        // Explanation: privateVar is not accessible directly because it's declared as private
+        // System.out.println("Private variable in SuperClass: " + privateVar);
+        System.out.println("Protected variable in SuperClass: " + protectedVar);
+        System.out.println("Public variable in SuperClass: " + publicVar);
     }
 
     // 2.1.4. Overloaded methods
     void overloadedMethod() {
-        System.out.println("Inside SubClass's overloadedMethod()");
+        System.out.println("Overloaded method in SubClass");
     }
 
     void overloadedMethod(int num) {
-        System.out.println("Inside SubClass's overloadedMethod(int num)");
+        System.out.println("Overloaded method in SubClass with parameter: " + num);
     }
 
-    // 2.1.5. Overridden methods
+    // 2.1.5. Override methods
     @Override
-    void overriddenMethod() {
-        System.out.println("Inside SubClass's overriddenMethod()");
+    public void display() {
+        System.out.println("Inside SubClass");
     }
 
-    // 2.1.6. Use of super keyword to call overridden method
-    @Override
-    void overriddenMethod() {
-        super.overriddenMethod();
-        System.out.println("Inside SubClass's overriddenMethod()");
+    // 2.1.6. Use of super keyword
+    void callSuperMethod() {
+        super.display(); // Calling overridden method of superclass
     }
 
-    // 2.1.8. Implement abstract method
-    @Override
-    void abstractMethod() {
-        System.out.println("Inside SubClass's abstractMethod()");
+    // 2.1.7. Use of super keyword for parent constructor call
+    SubClass() {
+        super(); // Calling superclass constructor
+    }
+
+    SubClass(int num) {
+        super(); // Calling superclass constructor
+        System.out.println("Non-default constructor in SubClass: " + num);
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        // 2.1.2. Create an instance of the subclass
-        SuperClass a = new SubClass(10, "Hello", 3.14);
+        // 2.1.2. Create subclass
+        SuperClass a = new SubClass();
+        // This is possible because the subclass inherits all the members of its superclass.
 
-        // 2.1.3. Demonstrate accessing superclass instance variables
-        // System.out.println(a.privateVar); // Error: privateVar has private access in SuperClass
-        System.out.println(a.protectedVar); // OK: Protected access allows access within subclass
-        System.out.println(a.publicVar); // OK: Public access allows access from any class
+        // 2.1.2. Print result of getClass()
+        System.out.println("Class of instance 'a': " + a.getClass());
 
-        // 2.1.3. Demonstrate explanation when no access to superclass member
-        // System.out.println(a.privateVar); // Error: privateVar has private access in SuperClass
+        // 2.1.3. Access superclass instance variables inside child class methods
+        SubClass sub = new SubClass();
+        sub.accessVariables();
 
         // 2.1.4. Call overloaded methods
-        SubClass b = new SubClass(20, "World", 6.28);
-        b.overloadedMethod();
-        b.overloadedMethod(5);
+        SubClass subClass = new SubClass();
+        subClass.overloadedMethod();
+        subClass.overloadedMethod(5);
 
         // 2.1.5. Call overridden method
-        a.overriddenMethod(); // Calls SubClass's overriddenMethod()
+        SubClass subClass1 = new SubClass();
+        subClass1.display(); // Calls overridden method
 
-        // 2.1.6. Use of super keyword to call overridden method
-        b.overriddenMethod();
+        // 2.1.6. Use of super keyword
+        SubClass subClass2 = new SubClass();
+        subClass2.callSuperMethod(); // Calls superclass method
 
-        // 2.1.8. Demonstrate abstract classes and methods
-        b.abstractMethod();
+        // 2.1.7. Use of super keyword for parent constructor call
+        SubClass subClass3 = new SubClass(10); // Calls non-default superclass constructor
     }
 }
